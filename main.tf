68dcb912cefa9863306efd612bf6cce500c359e4
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-02-11 21:57:51 -0600

    add customer managed keys for aws s3 encryption in sse-c

--- main.tf
+++ main.tf
@@ -16,3 +16,24 @@ resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_kms_key" "s3_cmk" {
+  description             = "Customer managed key for S3 bucket encryption"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Name = "${var.s3_bucket_name}-cmk"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
+  bucket = aws_s3_bucket.s3.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = aws_kms_key.s3_cmk.arn
+    }
+  }
+}
