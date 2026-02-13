d46231f75406886b614533885ba3dc9cd4482065
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-02-13 16:42:40 -0600

    fix: use correct resource name for oac

--- cloudfront.tf
+++ cloudfront.tf
@@ -10,7 +10,7 @@ resource "aws_cloudfront_origin_access_control" "default_oac_s3" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = aws_s3_bucket.s3.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_access_control_id = aws_cloudfront_origin_access_control.default_oac_s3.id
     origin_id                = local.s3_origin_id
   }
 
