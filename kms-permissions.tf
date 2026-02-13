0fb4a69439b235a994083b796037e2fc41f386bb
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-02-13 17:03:18 -0600

    run terraform fmt

--- kms-permissions.tf
+++ kms-permissions.tf
@@ -1,7 +1,7 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_kms_key_policy" "kms_key_policy" {
-  depends_on = [ aws_cloudfront_distribution.s3_distribution ]
+  depends_on = [aws_cloudfront_distribution.s3_distribution]
 
   key_id = aws_kms_key.s3_cmk.id
   policy = jsonencode({
