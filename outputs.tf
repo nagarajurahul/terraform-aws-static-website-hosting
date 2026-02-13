74a758a1b7d68e9405d15889c7ba3365a4f2e857
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-02-13 10:41:54 -0600

    add outputs for cloudfront

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "s3_bucket_regional_domain_name" {
 output "index_file_arn" {
   value = aws_s3_object.index_file.arn
 }
+
+output "cloudfront_arn" {
+  value = aws_cloudfront_distribution.s3_distribution.arn
+}
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.s3_distribution.domain_name
+}
+
+output "cloudfront_status" {
+  value = aws_cloudfront_distribution.s3_distribution.status
+}
