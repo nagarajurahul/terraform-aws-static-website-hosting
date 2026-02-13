f0c4d65b31e1d32135ec13239b8f2e0fe894e6c2
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-02-13 10:41:40 -0600

    fix: missing permissions to solve access denied error - add cloudfront to access kms key for decrypting buckets and its objects

---
+++ kms-permissions.tf
@@ -0,0 +1,36 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_kms_key_policy" "kms_key_policy" {
+  key_id = aws_kms_key.s3_cmk.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      },
+      {
+        Sid    = "Allow CloudFront to decrypt"
+        Effect = "Allow"
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+        Action = [
+          "kms:Decrypt",
+          "kms:GenerateDataKey"
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.s3_distribution.arn
+          }
+        }
+      }
+    ]
+  })
+}
