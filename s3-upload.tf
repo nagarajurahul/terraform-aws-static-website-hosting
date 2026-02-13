a34d28ace2982d2b3718bbd44f2be01bbfdcf48d
Rahul Nagaraju <rahulnagaraju724@gmail.com>
2026-02-12 23:20:29 -0600

    add comment for best practice for s3 upload file

--- s3-upload.tf
+++ s3-upload.tf
@@ -1,4 +1,5 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
+# This is best to do in CI/CD, as any new changes in html file wont trigger terraform apply again, as terraform doesnt track html files
 resource "aws_s3_object" "index_file" {
   key        = "index.html"
   bucket     = aws_s3_bucket.s3.id
